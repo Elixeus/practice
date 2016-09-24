18c021fa7b12e3a069c5f9b0da6b03bdeaf6faaf
Elixeus <xw876@nyu.edu>
2016-09-24 18:19:45 -0400

    for loop

---
+++ LotteryOdds.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class LotteryOdds
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("How many numbers do you need to draw?");
+        int k = in.nextInt();
+
+        System.out.println("What is the highest number?");
+        int n = in.nextInt();
+
+        int lotteryOdds = 1;
+        for (int i = 1; i < k; i++)
+        {
+            lotteryOdds = lotteryOdds * (n - i + 1) / i;
+            
+        }
+        double f = lotteryOdds;
+        System.out.println("Your winning odds are 1 in " + lotteryOdds + ". That is " + 1/f + ". Good luck!");
+    }
+}
